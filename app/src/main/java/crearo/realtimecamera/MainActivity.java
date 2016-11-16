a0483686b080ef3794b3ab65c003ac1a8b9ad572
crearo <bhardwaj.rish@gmail.com>
2016-11-17 03:41:35 +0530

    Init Commit with OpenCV libs loaded

---
+++ app/src/main/java/crearo/realtimecamera/MainActivity.java
@@ -0,0 +1,42 @@
+package crearo.realtimecamera;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Load successfully");
+        } else {
+            Log.d(TAG, "Not loaded");
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
