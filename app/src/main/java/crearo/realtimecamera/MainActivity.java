deaa1ae1cb0c628e7d35d2cf63c1b6cd7267a65e
crearo <bhardwaj.rish@gmail.com>
2016-11-17 04:04:23 +0530

    Basic camera using javacameraview

--- app/src/main/java/crearo/realtimecamera/MainActivity.java
+++ app/src/main/java/crearo/realtimecamera/MainActivity.java
@@ -3,30 +3,90 @@ package crearo.realtimecamera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.TextView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static String TAG = MainActivity.class.getSimpleName();
 
-    static {
-        if (OpenCVLoader.initDebug()) {
-            Log.d(TAG, "Load successfully");
-        } else {
-            Log.d(TAG, "Not loaded");
+    private JavaCameraView javaCameraView;
+    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case BaseLoaderCallback.SUCCESS:
+                    javaCameraView.enableView();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
         }
-    }
+    };
+
+    Mat matRgba, matGray, matCanny;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        javaCameraView = (JavaCameraView) findViewById(R.id.java_camera_view);
+        javaCameraView.setCvCameraViewListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Load successfully");
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        } else {
+            Log.d(TAG, "Not loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, baseLoaderCallback);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (javaCameraView != null)
+            javaCameraView.disableView();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (javaCameraView != null)
+            javaCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        matRgba = new Mat(height, width, CvType.CV_8UC4);
+        matGray = new Mat(height, width, CvType.CV_8UC1);
+        matCanny = new Mat(height, width, CvType.CV_8UC1);
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        matRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        matRgba = inputFrame.rgba();
+        Imgproc.cvtColor(matRgba,matGray,Imgproc.COLOR_RGBA2GRAY);
+        return matGray;
     }
 
     /**
